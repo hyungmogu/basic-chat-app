478115d1f62490bb74a740153df62b6f43da0f01
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-13 15:52:44 -0700

    Add progress in building screen 'LoginScreen'   - Add textinput 'username' and stylize login inputs

--- basic-chat-app/screens/LoginScreen.js
+++ basic-chat-app/screens/LoginScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,  } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.logo}>Basic&nbsp; Chat App</Text>
+        <Text style={styles.logo}>Basic&nbsp; Chat App</Text>
+        <TextInput style={styles.input} placeholder={'Username'} placeholderTextColor="white"></TextInput>
     </View>
   );
 }
@@ -14,12 +16,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   logo: {
     width: 250,
     fontSize: 50,
     fontWeight: 'bold',
     textAlign: 'center'
+  },
+  input: {
+      borderRadius: 10,
+      borderWidth: 1,
+      backgroundColor: '#D6D6D6',
+      borderColor: '#E2E2E2',
+      padding: 15,
+      width: 350
   }
 });

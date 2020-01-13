a3b5731e47ef9c061b6f279e8731880100df6fa9
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-13 16:58:31 -0700

    Add progress building screen 'LoginScreen'   - Add spacings between logo and inputs

--- basic-chat-app/screens/LoginScreen.js
+++ basic-chat-app/screens/LoginScreen.js
@@ -5,9 +5,11 @@ import { TextInput } from 'react-native-gesture-handler';
 export default function App() {
   return (
     <View style={styles.container}>
-        <Text style={styles.logo}>Basic&nbsp; Chat App</Text>
-        <TextInput style={styles.loginInput} placeholder={'Username'} placeholderTextColor="white"></TextInput>
-        <TextInput style={styles.loginInput} secureTextEntry={true} placeholder={'Password'} placeholderTextColor="white"></TextInput>
+        <Text style={styles.logo}>Basic{"\n"} Chat App</Text>
+        <View style={styles.inputContainer}>
+            <TextInput style={styles.input} placeholder={'Username'} placeholderTextColor="white"></TextInput>
+            <TextInput style={styles.input} secureTextEntry={true} placeholder={'Password'} placeholderTextColor="white"></TextInput>
+        </View>
     </View>
   );
 }
@@ -21,11 +23,12 @@ const styles = StyleSheet.create({
   },
   logo: {
     width: 250,
-    fontSize: 50,
+    fontSize: 45,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    marginBottom: 30
   },
-  loginInput: {
+  input: {
       borderRadius: 10,
       borderWidth: 1,
       backgroundColor: '#D6D6D6',

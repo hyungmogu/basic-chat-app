2b7f687e7553539bd5a0b842f951173da5aa9c3a
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-13 18:08:15 -0700

    Add progress building screen 'SignUpScreen'   - Add navigation to signup page

--- basic-chat-app/navigation/AppNavigator.js
+++ basic-chat-app/navigation/AppNavigator.js
@@ -2,9 +2,11 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
 import LoginScreen from '../screens/LoginScreen';
+import SignUpScreen from '../screens/SignUpScreen';
 
 const MainAppNavigation = createStackNavigator({
-    Login: {screen: LoginScreen}
+    Login: {screen: LoginScreen},
+    SignUp: {screen: SignUpScreen}
 },
 {
     initialRouteName: 'Login',

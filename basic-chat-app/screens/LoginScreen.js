8b46ec4c4c43b690a0f305d3b10123f92e3a8b5b
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-13 18:41:35 -0700

    Add progress building screen 'SignUpScreen, LoginScreen'   - Place logos on the same height from screen

--- basic-chat-app/screens/LoginScreen.js
+++ basic-chat-app/screens/LoginScreen.js
@@ -1,69 +1,75 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, SafeAreaView, Dimensions } from 'react-native';
 
 export default class LoginScreen extends Component {
     render() {
         const {navigate} = this.props.navigation;
         return (
-            <View style={styles.container}>
-                <Text style={styles.logo}>Basic{"\n"} Chat App</Text>
-                <View style={styles.inputContainer}>
-                    <TextInput style={styles.input} placeholder={'Username'} placeholderTextColor="black"></TextInput>
-                    <TextInput style={styles.input} secureTextEntry={true} placeholder={'Password'} placeholderTextColor="black"></TextInput>
+            <SafeAreaView style={styles.safeViewContainer}>
+                <View style={styles.container}>
+                    <Text style={styles.logo}>Basic{"\n"} Chat App</Text>
+                    <View style={styles.inputContainer}>
+                        <TextInput style={styles.input} placeholder={'Username'} placeholderTextColor="black"></TextInput>
+                        <TextInput style={styles.input} secureTextEntry={true} placeholder={'Password'} placeholderTextColor="black"></TextInput>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                            <Text>Login</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.button, styles.buttonSecondary]} onPress={() => navigate('SignUp')}>
+                            <Text>Sign Up</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
-                        <Text>Login</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.button, styles.buttonSecondary]} onPress={() => navigate('SignUp')}>
-                        <Text>Sign Up</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            </SafeAreaView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  logo: {
-    width: 250,
-    fontSize: 45,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 30
-  },
-  input: {
-      borderRadius: 10,
-      borderWidth: 1,
-      backgroundColor: '#E2E2E2',
-      borderColor: '#E2E2E2',
-      padding: 15,
-      width: 350,
-      marginBottom: 5
-  },
-  inputContainer: {
-      marginBottom: 15
-  },
-  button: {
-      padding: 15,
-      borderRadius: 10,
-      fontSize: 20,
-      width: 350,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginBottom: 5
-  },
-  buttonPrimary: {
-      backgroundColor: '#E2E2E2'
-  },
-  buttonSecondary: {
-      backgroundColor: '#D6D6D6'
-  }
+    safeViewContainer: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: Dimensions.get('window').height / 6,
+        alignItems: 'center'
+    },
+    logo: {
+        width: 250,
+        fontSize: 45,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 30
+    },
+    input: {
+        borderRadius: 10,
+        borderWidth: 1,
+        backgroundColor: '#E2E2E2',
+        borderColor: '#E2E2E2',
+        padding: 15,
+        width: 350,
+        marginBottom: 5
+    },
+    inputContainer: {
+        marginBottom: 15
+    },
+    button: {
+        padding: 15,
+        borderRadius: 10,
+        fontSize: 20,
+        width: 350,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 5
+    },
+    buttonPrimary: {
+        backgroundColor: '#E2E2E2'
+    },
+    buttonSecondary: {
+        backgroundColor: '#D6D6D6'
+    }
 });

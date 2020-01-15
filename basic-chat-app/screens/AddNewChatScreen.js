071c8faa437b78e6daef40cd3f5a963177f95fd0
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-15 16:58:19 -0700

    Add progress building screen 'Chat Screen'   - Hook add new chat screen to chat screen

--- basic-chat-app/screens/AddNewChatScreen.js
+++ basic-chat-app/screens/AddNewChatScreen.js
@@ -11,7 +11,7 @@ export default class AddNewChatScreen extends Component {
             <SafeAreaView style={styles.safeViewContainer}>
                 <View style={styles.container}>
                     <AppInput placeholder={'Email'}/>
-                    <AppButton type={"secondary"}>Start New Chat</AppButton>
+                    <AppButton type={"secondary"} onPress={() => navigate('Chat')}>Start New Chat</AppButton>
                 </View>
             </SafeAreaView>
         );

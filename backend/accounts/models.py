f1e9774d7893cbf7820fef4b6d09d1c939db0c5f
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-20 19:12:24 -0700

    Add progress 'backend'   - Add model 'User'

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+import main.models as mainModel
+
+class User (AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    profile_picture = models.URLField(blank=True, null=True)
+
+    # objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return "{}".format(self.email)

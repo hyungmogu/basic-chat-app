5c55067e77ebbd981324305227ede02c4ddaa849
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-20 17:25:12 -0700

    Add progress 'backend'   - add model 'chatroom'

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.conf import settings
 
+
 class ChatRoom(models.Model):
-    lastest_chat=models.ForeignKey('Chat', related_name='latest_chat')
+    user1 = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user2 = models.ForeignKey(settings.AUTH_USER_MODEL)
+    last_chat = models.ForeignKey('Chat')
+
 
 class Chat(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True, blank=True)
     text = models.TextField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    chat_room = models.ForeignKey('ChatRoom')
 
     def __str__(self):
         return self. "{} {}: {}".format(timestamp, user, text)
+
+
+
 

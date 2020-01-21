53b360c160a108a323fbfc1014c47f35097d3ffe
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-20 18:27:39 -0700

    Add progress 'backend'   - Add on_delete rule to models

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 from django.conf import settings
 
-
 class ChatRoom(models.Model):
-    user1 = models.ForeignKey(settings.AUTH_USER_MODEL)
-    user2 = models.ForeignKey(settings.AUTH_USER_MODEL)
-    last_chat = models.ForeignKey('Chat')
-
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    last_chat = models.ForeignKey('Chat', on_delete=models.SET_NULL)
 
 class Chat(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True, blank=True)
     text = models.TextField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    chat_room = models.ForeignKey('ChatRoom')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
+    chat_room = models.ForeignKey('ChatRoom', on_delete=models.CASCADE)
 
     def __str__(self):
         datetime = self.timestamp.strftime('%Y-%m-%d %H:%M')

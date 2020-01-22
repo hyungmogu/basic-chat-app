d97aa405c070fe6a79cfeed4f5fe5a75f8042bf9
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-21 19:26:06 -0700

    Add progress 'backend'   - add api urls '/api/v1/login/' and '/api/v1/logout/'

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 app_name = 'api'
 urlpatterns = [
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
 ]

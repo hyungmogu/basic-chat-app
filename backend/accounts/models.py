fb9e56ead72ef6aa464164c04e785899a0244c00
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-21 18:38:46 -0700

    Add progress 'backend'   - customize user creation logics

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,7 +3,34 @@ from django.utils import timezone
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-import main.models as mainModel
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, name, password=None):
+
+        if not email:
+            raise ValueError("User must have a valid email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username)
+        user.set_password(password)
+        user.save()
+
+        self.create_user_profile(user)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.save()
+
+        self.create_user_profile(user)
+
+        return user
 
 
 class User (AbstractBaseUser, PermissionsMixin):
@@ -11,7 +38,7 @@ class User (AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=100)
     profile_picture = models.URLField(blank=True, null=True)
 
-    # objects = UserManager()
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']

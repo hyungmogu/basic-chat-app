4e084fb91d0012be6fcb491ddc3e61ebc8c49c12
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-21 18:20:43 -0700

    Add progress 'backend'   - resolve "The field named as the 'USERNAME_FIELD' for a custom user model must not be included in 'REQUIRED_FIELDS'" error

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 import main.models as mainModel
 
+
 class User (AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
@@ -13,7 +14,7 @@ class User (AbstractBaseUser, PermissionsMixin):
     # objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['email', 'name']
+    REQUIRED_FIELDS = ['name']
 
     def __str__(self):
         return "{}".format(self.email)

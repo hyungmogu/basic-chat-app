f8c5d199594848cec2ccc89e010b7ccc40282cc4
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-22 14:41:11 -0700

    Add progress 'backend'   - define list of todos

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,19 @@
-from django.contrib.auth import get_user_model
+"""
+@TODO
+- List of API endpoints to define
+
+[]: Login (POST)
+[]: Logout (GET)
+[]: Signup (POST)
+[]: Get all chatrooms (GET)
+[]: Get all chats in a given chatroom (GET)
+[]: Modify name of user (POST)
+[]: Modify profile of user (POST)
+
+"""
+
+
+from django.contrib.auth import get_user_model, login, authenticate
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,12 +23,16 @@ class Login(APIView):
     def post(self, request, format=None):
 
         # 1. Parse data from request
+        serializer = serializers.LocationSerializer(data=request.data)
 
-        # 2. use django.contrib.auth to authenticate
+        # 2. validate serialized data
+        serializer.is_valid(raise_exception=True)
 
-        # 3. if not successful, return response with error
+        # 3. use django.contrib.auth to authenticate
 
-        # 4. if successful, return response with success message
+        # 4. if not successful, return response with error
+
+        # 5. if successful, return response with success message
 
         pass
 

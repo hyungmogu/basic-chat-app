6077920793c907d669a443f620254c028685d87e
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-22 14:10:24 -0700

    Add progress 'backend'   - lay pseudocode 'api/v1/login'

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,2 @@
+from rest_framework.views import APIView
 from django.shortcuts import render
-
-# Create your views here.
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from accounts.models import AccountsModel
+
+
+class Login(APIView):
+    def post(self, request, format=None):
+
+        # 1. Parse data from request
+
+        # 2. use django.contrib.auth to authenticate
+
+        # 3. if not successful, return response with error
+
+        # 4. if successful, return response with success message
+
+        return Response (serializer.data)

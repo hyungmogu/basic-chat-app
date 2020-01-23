57960552c0c26d169a5e9480e09890feaf61a4e8
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-22 17:12:20 -0700

    Add progress 'backend'   - clean code 'Login'

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,23 +10,17 @@ from accounts.serializer import UserSerializer
 
 class Login(APIView):
     def post(self, request, format=None):
-
-        # 1. Parse data from request
         email = request.data['email']
         password = request.data['password']
 
-        # 2. use django.contrib.auth to authenticate
         user = authenticate(email=email, password=password)
 
-        # 3. if not successful, return response with error
         if not user:
             error = {
                 'error': 'User does not exist / Password is incorrect'
             }
 
             return Response(error, status=status.HTTP_400_BAD_REQUEST)
-
-        # 4. if successful, return response with user info
 
         user_serializer = UserSerializer(user)
 

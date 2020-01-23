9360ae6da3d5b17073bfd523310fea696d57ec0b
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-22 18:07:51 -0700

    Add progress 'backend'   - Add url 'api/v1/signup'

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,5 @@ app_name = 'api'
 urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
 ]

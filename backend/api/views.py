b85a6846db2a2c86e4e3e87d4ba679bb84e8e994
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-23 14:15:13 -0700

    Add progress 'backend'   - return auth token on login 'api/v1/login/'

--- backend/api/views.py
+++ backend/api/views.py
@@ -34,12 +34,13 @@ class Login(APIView):
 
             return Response(error, status=status.HTTP_400_BAD_REQUEST)
 
-        token = Token.objects.create(user=user)
+        token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
 
-        print(user_serializer.data)
+        res_data = user_serializer.data
+        res_data['auth_token'] = token.key
 
-        return Response(user_serializer.data)
+        return Response(res_data)
 
 
 class Logout(APIView):

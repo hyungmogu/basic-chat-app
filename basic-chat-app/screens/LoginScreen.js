3e41b1fedcc4c0539e17b927218d7b342271fd9f
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-29 22:53:49 -0700

    Add progress 'mobile'   - add pseudocode   - hook login endpoint to screen

--- basic-chat-app/screens/LoginScreen.js
+++ basic-chat-app/screens/LoginScreen.js
@@ -6,66 +6,77 @@ import {
     KeyboardAvoidingView
 } from 'react-native';
 
+import axios from 'axios';
+
+import { Consumer } from '../components/Context';
 import AppInput from '../components/AppInput';
 import Logo from '../components/Logo';
 import AppButton from '../components/AppButton';
 
 export default class LoginScreen extends Component {
 
-    async componentDidMount() {
-        const { status } = await Camera.requestPermissionsAsync();
+    componentDidMount() {}
 
-        this.setState({
-            hasPermission: status === 'granted'
+    handleLogin = (email, password, storeUserInfo) => {
+        let data = JSON.stringify({
+            email: email,
+            password: password
+        })
+
+        // display spinner
+
+        axios.post('http://localhost:8000/api/v1/login', data).then( res => {
+            // add response to state at app level
+
+            // proceed to Home Screen
+        }).catch(err => {
+            console.warn(err);
+        }).finally(_ => {
+            // close spinner
         });
-
-        try {
-            let photos = await FileSystem.readDirectoryAsync(this.state.photosPath);
-
-            if (photos && Array.isArray(photos)) {
-                this.setState({
-                photos: [...photos],
-                latestImage: Array.isArray(photos) ? photos[photos.length - 1] : []
-                });
-            }
-        } catch (Error) {}
     }
-
 
     render() {
         const {navigate} = this.props.navigation;
         return (
-            <SafeAreaView style={styles.safeViewContainer}>
-                <KeyboardAvoidingView
-                    style={styles.container}
-                    behavior = "padding"
-                    enabled
-                >
-                    <View style={styles.logoContainer}>
-                        <Logo/>
-                    </View>
-                    <View style={styles.inputContainer}>
-                        <AppInput placeholder={'Email'}/>
-                        <AppInput secureTextEntry={true} placeholder={'Password'}/>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <AppButton
-                            type={"primary"}
-                            style={{marginBottom: 5}}
-                            onPress={() => navigate('Home')}
-                        >
-                            Login
-                        </AppButton>
-                        <AppButton
-                            type={"secondary"}
-                            style={{marginBottom: 5}}
-                            onPress={() => navigate('SignUp')}
-                        >
-                            Sign Up
-                        </AppButton>
-                    </View>
-                </KeyboardAvoidingView>
-            </SafeAreaView>
+            <Consumer>
+                { context => {
+
+                    return(
+                        <SafeAreaView style={styles.safeViewContainer}>
+                            <KeyboardAvoidingView
+                                style={styles.container}
+                                behavior = "padding"
+                                enabled
+                            >
+                                <View style={styles.logoContainer}>
+                                    <Logo/>
+                                </View>
+                                <View style={styles.inputContainer}>
+                                    <AppInput placeholder={'Email'}/>
+                                    <AppInput secureTextEntry={true} placeholder={'Password'}/>
+                                </View>
+                                <View style={styles.buttonContainer}>
+                                    <AppButton
+                                        type={"primary"}
+                                        style={{marginBottom: 5}}
+                                        onPress={() => navigate('Home')}
+                                    >
+                                        Login
+                                    </AppButton>
+                                    <AppButton
+                                        type={"secondary"}
+                                        style={{marginBottom: 5}}
+                                        onPress={() => navigate('SignUp')}
+                                    >
+                                        Sign Up
+                                    </AppButton>
+                                </View>
+                            </KeyboardAvoidingView>
+                        </SafeAreaView>
+                    )
+                }}
+            </Consumer>
         );
     }
 }

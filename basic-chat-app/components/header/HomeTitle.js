b7f5ed70c63fc27175b54bcc4bf16c68ab5c6a15
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-30 15:13:38 -0700

    Add progress 'mobile'   - Reaplce 'Add name here' with name from login 'HomeTitle'

--- basic-chat-app/components/header/HomeTitle.js
+++ basic-chat-app/components/header/HomeTitle.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
+import { Consumer } from '../Context';
 import { withNavigation } from 'react-navigation';
 
 class HomeTitle extends Component {
     render() {
         const {navigate} = this.props.navigation;
         return (
-            <TouchableOpacity style={styles.item} onPress={() => navigate('User')}>
-                <Text style={styles.text}>User Name Goes Here</Text>
-            </TouchableOpacity>
+            <Consumer>
+                { context => {
+                    let name = context.name;
+
+                    return(
+                        <TouchableOpacity style={styles.item} onPress={() => navigate('User')}>
+                            <Text style={styles.text}>{name}</Text>
+                        </TouchableOpacity>
+                    );
+                }}
+            </Consumer>
         );
     }
 }

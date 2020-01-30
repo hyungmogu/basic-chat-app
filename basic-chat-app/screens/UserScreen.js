e0ef7800a4114eda146153336f695bbc029c7bb0
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-30 15:36:00 -0700

    Add progress 'mobile'   - Replace static name with user name 'UserScreen'

--- basic-chat-app/screens/UserScreen.js
+++ basic-chat-app/screens/UserScreen.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, SafeAreaView, Image, Text } from 'react-native';
 
+import { Consumer } from '../components/Context';
 import AppButton from '../components/AppButton';
 
 export default class UserScreen extends Component {
     render() {
         const {navigate} = this.props.navigation;
         return (
-            <SafeAreaView style={styles.safeViewContainer}>
-                <View style={styles.container}>
-                    <View style={styles.bodyContainer}>
-                        <Image
-                            style={styles.userImage}
-                            source={{uri: 'https://www.publicdomainpictures.net/pictures/200000/velka/plain-red-background.jpg' }}
-                        />
-                        <Text style={styles.name}>James</Text>
-                        <AppButton>Change Profile Picture</AppButton>
-                        <AppButton>Change Name</AppButton>
-                    </View>
-                    <View style={styles.footerContainer}>
-                        <AppButton
-                            type={'secondary'}
-                            onPress={() => navigate('Login')}
-                        >
-                            Logout
-                        </AppButton>
-                    </View>
-                </View>
-            </SafeAreaView>
+            <Consumer>
+                { context => {
+                    let name = context.name;
+                    return (
+                        <SafeAreaView style={styles.safeViewContainer}>
+                            <View style={styles.container}>
+                                <View style={styles.bodyContainer}>
+                                    <Image
+                                        style={styles.userImage}
+                                        source={{uri: 'https://www.publicdomainpictures.net/pictures/200000/velka/plain-red-background.jpg' }}
+                                    />
+                                    <Text style={styles.name}>{ name }</Text>
+                                    <AppButton>Change Profile Picture</AppButton>
+                                    <AppButton>Change Name</AppButton>
+                                </View>
+                                <View style={styles.footerContainer}>
+                                    <AppButton
+                                        type={'secondary'}
+                                        onPress={() => navigate('Login')}
+                                    >
+                                        Logout
+                                    </AppButton>
+                                </View>
+                            </View>
+                        </SafeAreaView>
+                    );
+                }}
+            </Consumer>
         );
     }
 }

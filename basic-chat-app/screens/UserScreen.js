15124187fc8f3a93792ce4cb232734a4cf3a33ff
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-30 15:52:51 -0700

    Add progress 'mobile'   - setup handleLogout method 'User Screen'

--- basic-chat-app/screens/UserScreen.js
+++ basic-chat-app/screens/UserScreen.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, SafeAreaView, Image, Text } from 'react-native';
 
+import axios from 'axios';
+
 import { Consumer } from '../components/Context';
 import AppButton from '../components/AppButton';
 
 export default class UserScreen extends Component {
+
+    handleLogout = (authToken, resetUserInfo, navigate) => {
+        let opts = {
+            headers: {
+                Authorization: `Token ${authToken}`
+            }
+        }
+
+        axios.get('http://localhost:8000/api/v1/logout/', opts).then( res => {
+            resetUserInfo(res.data);
+            navigate('Login')
+        }).catch(err => {
+            console.warn(err);
+        });
+    }
+
     render() {
         const {navigate} = this.props.navigation;
         return (
             <Consumer>
                 { context => {
                     let name = context.name;
+                    let authToken = context.authToken;
                     let avatar = context.avatar || 'https://www.publicdomainpictures.net/pictures/200000/velka/plain-red-background.jpg';
+                    let resetUserInfo = context.actions.resetUserInfo;
                     return (
                         <SafeAreaView style={styles.safeViewContainer}>
                             <View style={styles.container}>
@@ -27,7 +47,11 @@ export default class UserScreen extends Component {
                                 <View style={styles.footerContainer}>
                                     <AppButton
                                         type={'secondary'}
-                                        onPress={() => navigate('Login')}
+                                        onPress={() => handleLogout(
+                                            authToken,
+                                            resetUserInfo,
+                                            navigate
+                                        )}
                                     >
                                         Logout
                                     </AppButton>

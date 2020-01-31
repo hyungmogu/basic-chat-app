7f62ca233b7b2085a60329f8f17a7c214fc417d0
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-30 17:15:07 -0700

    Add progress 'mobile'   - Add ChatContext, ChatProvider and ChatConsumer

--- basic-chat-app/components/Context/index.js
+++ basic-chat-app/components/Context/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const UserContext = React.createContext();
+const ChatContext = React.createContext();
 
 export const UserProvider = UserContext.Provider;
 export const UserConsumer = UserContext.Consumer;
+
+export const ChatProvider = ChatContext.Provider;
+export const ChatConsumer = ChatContext.Consumer;

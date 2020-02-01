79b32caa7dc22d6ba977c386607fa98f32255011
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-01 11:11:27 -0700

    Add progress 'mobile'   - Replace UserProvider and UserConsumer with ChatProvider and ChatConsumer 'Context'

--- basic-chat-app/components/Context/index.js
+++ basic-chat-app/components/Context/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
-const UserContext = React.createContext();
 const ChatContext = React.createContext();
-
-export const UserProvider = UserContext.Provider;
-export const UserConsumer = UserContext.Consumer;
+const ChatContext = React.createContext();
 
 export const ChatProvider = ChatContext.Provider;
 export const ChatConsumer = ChatContext.Consumer;

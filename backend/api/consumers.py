af255dc56ef2fce0504bd2b1f6c1a43d655f0672
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-04 08:21:28 -0700

    Add progress 'adding websockets'   - Add logics 'send' and 'receive'

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -9,7 +9,6 @@ class ChatBoxConsumer(AsyncWebsocketConsumer):
         self.chattee_pk = self.scope['url_route']['kwargs']['pk']
         self.chat_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
         await self.channel_layer.group_add(
             self.chat_group_name,
             self.channel_name
@@ -18,31 +17,57 @@ class ChatBoxConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(
             self.chat_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+    async def receive(self, data):
+        # parse data
+        data_json = json.loads(data)
 
-        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'chat_message',
-                'message': message
+                'type': 'send',
+                'data': data_json
             }
         )
 
     # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
+    async def send(self, event):
+        data = event['data']
+
+        user_recipient = self.get_user(user_recipient_pk)
+
+        serializer = ChatBoxSerializer(data=data)
+
+        # Send message to room group
+        chatbox = self.create_chatbox(request.user, user_recipient, text)
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        await self.send(text_data=json.dumps(serializer.data))
+
+    async def create_chatbox(self, user, user_recipient, text):
+
+        chatbox = ChatBoxModel.objects.create(
+            text=text,
+            msg_from=user,
+            msg_to=user_recipient
+        )
+
+        return chatbox
+
+    async def get_user(self, user_pk):
+        User = get_user_model()
+
+        user = None
+
+        try:
+            user = User.objects.get(pk=user_pk)
+        except (ObjectDoesNotExist):
+            pass
+
+        return user
+

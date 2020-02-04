f4688dc6e24fd054bf905b9693b1184da35baf30
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-03 23:53:48 -0700

    install and setup django channels

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,3 +153,15 @@ NOSE_ARGS = [
     # '--with-coverage',
     # '--cover-package=foo,bar',
 ]
+
+
+# Channels
+ASGI_APPLICATION = "backend.routing.application"
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}

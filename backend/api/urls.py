19951b29686581cecb7d7dfdf17cdef89c03ce64
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-06 14:31:58 -0700

    Add progress 'create change profile picture'   - Add url for photo upload

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('chats/', views.Chats.as_view(), name='chats'),
     path('chats/<int:pk>', views.ChatBox.as_view(), name='chat'),
+    path('chats/', views.Chats.as_view(), name='chats'),
+    path('photo/', views.Photo.as_view(), name='photo')
 ]

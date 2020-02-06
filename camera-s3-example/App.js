94c2e3a9aed5c66dc9783d7dee8e15d8646980f8
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-06 14:00:22 -0700

    Add progress 'create change profile picture'   - Add method handleConvertBase64ToBlob

--- camera-s3-example/App.js
+++ camera-s3-example/App.js
@@ -58,6 +58,28 @@ export default class App extends Component {
       })
   }
 
+  handleConvertBase64ToBlob = (dataURI) => {
+    // source: https://gist.github.com/fupslot/5015897
+
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    let byteString = atob(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    // write the bytes of the string to an ArrayBuffer
+    let ab = new ArrayBuffer(byteString.length);
+    let ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    // write the ArrayBuffer to a blob, and you're done
+    let bb = new Blob([ab]);
+    return bb;
+  }
+
   render() {
       let permission = this.state.hasPermission;
       let type = this.state.cameraType;

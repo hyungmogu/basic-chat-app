7db1a0487c07b06bb2886b50f13eccb4c86a1c2f
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-08 00:58:51 -0700

    Add progress 'Add image edit screen'   - Add codes from facebook's image edit example to image edit screen 'camera-s3-example'

--- camera-s3-example/screens/ImageEditScreen.js
+++ camera-s3-example/screens/ImageEditScreen.js
@@ -6,30 +6,139 @@ import {
 
 export default class ImageEditScreen extends Component {
 
-  render() {
-        let image = this.props.navigation.state.params.image;
+    state = {
+        photo: {
+            uri: `https://source.unsplash.com/2Ts5HnA67k8/${DEFAULT_IMAGE_WIDTH}x${DEFAULT_IMAGE_HEIGHT}`,
+            height: DEFAULT_IMAGE_HEIGHT,
+            width: DEFAULT_IMAGE_WIDTH,
+        },
+        measuredSize: null,
+        croppedImageURI: null,
+        cropError: null
+    };
+
+    render() {
+        if (!this.state.measuredSize) {
+            return (
+            <View
+                style={styles.container}
+                onLayout={event => {
+                const measuredWidth = event.nativeEvent.layout.width;
+                if (!measuredWidth) {
+                    return;
+                }
+                this.setState({
+                    measuredSize: {width: measuredWidth, height: measuredWidth},
+                });
+                }}
+            />
+            );
+        }
+
+        if (!this.state.croppedImageURI) {
+            return this._renderImageCropper();
+        }
+        return this._renderCroppedImage();
+    }
+
+    _renderImageCropper() {
+        if (!this.state.photo) {
+          return <View style={styles.container} />;
+        }
+        let error = null;
+        if (this.state.cropError) {
+          error = <Text>{this.state.cropError.message}</Text>;
+        }
         return (
-            <SafeAreaView style={styles.container}>
-                <Image
-                    source={{uri: image.uri}}
-                    resizeMode={'contain'}
-                    style={styles.image}
-                />
-            </SafeAreaView>
+          <View style={styles.container}>
+            <Text style={styles.text} testID={'headerText'}>
+              Drag the image within the square to crop:
+            </Text>
+            <ImageCropper
+              image={this.state.photo}
+              size={this.state.measuredSize}
+              style={[styles.imageCropper, this.state.measuredSize]}
+              onTransformDataChange={data => (this._transformData = data)}
+            />
+            <TouchableHighlight
+              style={styles.cropButtonTouchable}
+              onPress={this._crop.bind(this)}>
+              <View style={styles.cropButton}>
+                <Text style={styles.cropButtonLabel}>Crop</Text>
+              </View>
+            </TouchableHighlight>
+            {error}
+          </View>
         );
-  }
+    }
+
+    _renderCroppedImage() {
+        return (
+            <View style={styles.container}>
+            <Text style={styles.text}>Here is the cropped image:</Text>
+            <Image
+                source={{uri: this.state.croppedImageURI}}
+                style={[styles.imageCropper, this.state.measuredSize]}
+            />
+            <TouchableHighlight
+                style={styles.cropButtonTouchable}
+                onPress={this._reset.bind(this)}>
+                <View style={styles.cropButton}>
+                <Text style={styles.cropButtonLabel}>Try again</Text>
+                </View>
+            </TouchableHighlight>
+            </View>
+        );
+    }
+
+    async _crop() {
+        try {
+            const croppedImageURI = await ImageEditor.cropImage(
+            this.state.photo.uri,
+            this._transformData,
+            );
+
+            if (croppedImageURI) {
+            this.setState({croppedImageURI});
+            }
+        } catch (cropError) {
+            this.setState({cropError});
+        }
+    }
+
+    _reset() {
+        this.setState({
+            croppedImageURI: null,
+            cropError: null,
+        });
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'black'
+      flex: 1,
+      alignSelf: 'stretch',
+      marginTop: 60,
     },
-    image: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height
-    }
-});
+    imageCropper: {
+      alignSelf: 'center',
+      marginTop: 12,
+    },
+    cropButtonTouchable: {
+      alignSelf: 'center',
+      marginTop: 12,
+    },
+    cropButton: {
+      padding: 12,
+      backgroundColor: 'blue',
+      borderRadius: 4,
+    },
+    cropButtonLabel: {
+      color: 'white',
+      fontSize: 16,
+      fontWeight: '500',
+    },
+    text: {
+      color: 'white',
+    },
+  });

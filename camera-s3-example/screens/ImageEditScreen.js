60fec9f3795e6f598125c4c54287fb39c94235b4
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-08 00:20:51 -0700

    Add progress 'Add image edit screen'   - show image on image edit screen 'camera-s3-example'

--- camera-s3-example/screens/ImageEditScreen.js
+++ camera-s3-example/screens/ImageEditScreen.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-import { StatusBar, Text, View, SafeAreaView, TouchableOpacity, StyleSheet, Dimensions, Alert } from 'react-native';
+import {
+    Dimensions, Image,
+    SafeAreaView, StyleSheet
+} from 'react-native';
 
 export default class ImageEditScreen extends Component {
 
   render() {
-      return (
-        <SafeAreaView style={styles.container}>
-            <Text>Hello</Text>
-        </SafeAreaView>
-      );
+        let image = this.props.navigation.state.params.image;
+        return (
+            <SafeAreaView style={styles.container}>
+                <Image
+                    source={{uri: image}}
+                    resizeMode={'contain'}
+                    style={styles.image}
+                />
+            </SafeAreaView>
+        );
   }
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'black'
+    },
+    image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height
+    }
 });

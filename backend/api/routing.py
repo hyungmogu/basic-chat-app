dc6fd40c9926c93362df60970fe0bdf0877518ae
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-11 14:53:07 -0700

    Add progress 'create websocket connections between users using AWS'   - refine urls in config.js and routing.js     - done to match aws application load balancer

--- backend/api/routing.py
+++ backend/api/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'api/v1/ws/chats/(?P<user_pk>\d+)/$', consumers.ChatBoxConsumer),
+    re_path(r'/ws/api/v1/chats/(?P<user_pk>\d+)/$', consumers.ChatBoxConsumer),
 ]

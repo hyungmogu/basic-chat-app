17cbf0aa0a2cc7b5783aad3f2ad7c15bd66354ad
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-12 02:05:54 -0700

    Add progress 'create websocket connections between users using AWS'   - Change host and hostWS back to chat-application.hyungmogu.com

--- mobile/Config.js
+++ mobile/Config.js
@@ -1,5 +1,5 @@
 export default Config = {
-    host: 'http://chat-application-env.jmfttyavkw.us-west-2.elasticbeanstalk.com',
-    hostWs: 'ws://chat-application-env.jmfttyavkw.us-west-2.elasticbeanstalk.com:5000',
+    host: 'http://chat-application.hyungmogu.com',
+    hostWs: 'ws://chat-application.hyungmogu.com',
     defaultAvatar: 'https://hyungmogu-portfolio-site.s3-us-west-2.amazonaws.com/chat-application/user-icon.png'
 };
